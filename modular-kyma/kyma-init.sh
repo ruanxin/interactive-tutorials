8850f2065080c8cd417fc887aa872679675311b2
Piotr Bochyński <p.bochynski@gmail.com>
2023-03-08 11:06:06 +0100

    Update kyma-init.sh

--- modular-kyma/kyma-init.sh
+++ modular-kyma/kyma-init.sh
@@ -1,9 +1,7 @@
 curl -Lo kyma https://storage.googleapis.com/kyma-cli-unstable/kyma-linux # kyma-linux, kyma-linux-arm, kyma.exe, or kyma-arm.exe
 chmod +x kyma
 mv kyma /usr/local/bin/
-
 kyma alpha deploy --ci
-
 kubectl apply -f modules.yaml
-
+kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
 echo "Kyma Control Plane is ready. Go to next step."
