9169fd1537d646ff1adaea370b63343fd4920b17
Xin Ruan <xin.ruan@sap.com>
2023-05-02 16:05:57 +0200

    add v1beta2 demo

--- kyma-v1beta2/kyma-init.sh
+++ kyma-v1beta2/kyma-init.sh
@@ -1,6 +1,16 @@
 curl -Lo kyma https://storage.googleapis.com/kyma-cli-unstable/kyma-linux # kyma-linux, kyma-linux-arm, kyma.exe, or kyma-arm.exe
 chmod +x kyma
 mv kyma /usr/local/bin/
+export ISTIO_VERSION=1.17.1
+curl -L https://istio.io/downloadIstio | TARGET_ARCH=x86_64 sh -
+chmod +x istio-$ISTIO_VERSION/bin/istioctl
+mv istio-$ISTIO_VERSION/bin/istioctl /usr/local/bin
+istioctl install -y
+kubectl apply -f https://raw.githubusercontent.com/prometheus-community/helm-charts/main/charts/kube-prometheus-stack/crds/crd-servicemonitors.yaml
+
+kubectl create ns kcp-system
+kubectl create ns kyma-system
+
 kyma alpha deploy --ci -k  https://github.com/ruanxin/kyma-operator/config/control-plane?ref=v1beta2
 kubectl apply -f cluster-ip-module-template-fast.yaml
 kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
