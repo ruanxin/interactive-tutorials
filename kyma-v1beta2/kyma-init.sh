b07ed0e6bbe4268e9badd2125020a32a3018b3e5
Xin Ruan <xin.ruan@sap.com>
2023-05-02 16:53:00 +0200

    test

--- kyma-v1beta2/kyma-init.sh
+++ kyma-v1beta2/kyma-init.sh
@@ -10,8 +10,9 @@ kubectl apply -f https://raw.githubusercontent.com/prometheus-community/helm-cha
 
 kubectl create ns kcp-system
 kubectl create ns kyma-system
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.11.0/cert-manager.yaml
 
-kyma alpha deploy --ci -k  https://github.com/ruanxin/kyma-operator//config/control-plane?ref=v1beta2
+kubectl apply -f https://github.com/ruanxin/kyma-operator/config/control-plane?ref=v1beta2
 kubectl apply -f cluster-ip-module-template-fast.yaml
 kubectl taint nodes controlplane node-role.kubernetes.io/control-plane:NoSchedule-
 echo "Kyma Control Plane is ready. Go to next step."
